c6e74f88a25ec2767ae2f8396da3368d887cda24
WillowRussia <iv040606@gmail.com>
2025-09-14 17:25:30 +0300

    Initial Commit

---
+++ MVPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPTest
+//
+//  Created by Илья Востров on 14.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
